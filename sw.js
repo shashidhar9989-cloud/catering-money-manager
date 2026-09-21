3f9589da1520c81f4946a53a6895341271d0ef6c
shashidhar9989-cloud <shashidhar9989@gmail.com>
2026-09-21 15:47:02 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE='catering-money-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='catering-money-manager-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
